4c84db6215fa981b8a88c166e2da9bc9c6ff289b
simplon-marseille <acardnicolas@hotmail.com>
2019-05-13 20:41:34 +0200

    first commit

---
+++ gps.js
@@ -0,0 +1,33 @@
+//Les informations GPS sont stockées dans un hash.
+//Stockez ces informations dans un tableau.
+//Afficher les coordonnées GPS de chaque villes.
+
+const ville1 = {
+  nom: 'Paris',
+  longitude: '2.3522219',
+  latitude: '48.856614'
+};
+
+const ville2 = {
+  nom: 'Rome',
+  longitude: '12.496365',
+  latitude: '41.902783'
+};
+
+const ville3 = {
+  nom: 'Londres',
+  longitude: '-0.127758',
+  latitude: '51.507351'
+};
+
+const ville4 = {
+  nom: 'Marseille',
+  longitude: '5.369780',
+  latitude: '43.296482'
+};
+
+const ville5 = {
+  nom: 'Casablanca',
+  longitude: '-7.589843',
+  latitude: '33.573110'
+};
